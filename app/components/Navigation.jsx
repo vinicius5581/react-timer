7c6139aba5a4372025e26623af3d87ce889e07b0
Vini Santana <vini@administrators-MacBook-Pro.local>
2017-01-09 22:32:55 -0800

    Inital commit - Code author: Andrew Mead

---
+++ app/components/Navigation.jsx
@@ -0,0 +1,25 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Navigation = () => {
+  return (
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">
+            React Timer
+          </li>
+          <li>
+            <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
+          </li>
+          <li>
+            <Link to="/countdown" activeClassName="active-link">Countdown</Link>
+          </li>
+        </ul>
+      </div>
+      
+    </div>
+  );
+};
+
+module.exports = Navigation;
